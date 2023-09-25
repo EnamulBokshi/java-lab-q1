6a7fdf86c7ec7423fcf0514755ad4986ed1a708c
enamul hoque <Enamulbokshi@gmail.com>
2023-09-25 10:57:32 +0600

    Lab question 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args){
+        Vehicle vl = new Vehicle();
+        vl.drive();
+        Car cr = new Car();
+        cr.drive();
+    }
+}
